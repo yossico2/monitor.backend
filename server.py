fb698705dbbf7ba6c82865215093d2fc156bfc26
Yossi Cohen <yossico@rafael.co.il>
2022-06-20 15:37:12 +0300

    ref

--- server.py
+++ server.py
@@ -1,25 +1,51 @@
 import socketio
 import eventlet
+from time import time
+import random
 from datetime import datetime
+import json
 
 sio = socketio.Server()
 app = socketio.WSGIApp(sio)
+
 
 @sio.event
 def connect(sid, environ):
     print('connect ', sid)
 
-def generate_event():
+
+def generate_events():
+    stateNormal = 0
+    stateRequest = 1
+    stateResponse = 2
+    stateResolved = 3
+
+    stateArray = [
+        stateNormal,
+        stateRequest,
+        stateResponse,
+        stateResolved
+    ]
+
     while True:
-        message = f'lilo {datetime.now()}'
-        print(f'emit: {message}')
-        sio.emit('event', message)
-        sio.sleep(1)
+        event = {
+            'time': round(time(), 1),  # round to 100ms
+            'data': {
+                'state': stateArray[stateNormal],
+                'frequency': round(random.random() * 40000, 2)
+            }
+        }
+
+        sio.emit('event', json.dumps(event))
+
+        sio.sleep(0.1)
+
 
 @sio.event
 def disconnect(sid):
     print('disconnect ', sid)
 
+
 if __name__ == '__main__':
-    sio.start_background_task(generate_event)
+    sio.start_background_task(generate_events)
     eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
