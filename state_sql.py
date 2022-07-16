137ca973ddc427f04c2430e88f21de84cf2bde72
Yossi Cohen <yossico@rafael.co.il>
2022-07-16 08:04:27 +0200

    ref

--- state_sql.py
+++ state_sql.py
@@ -13,24 +13,43 @@ class StateSQL:
     def close(self):
         self.cnx.close()
 
-
     def init_db(self):
         '''
-        init db and tables if not exists
+        init db and table if not exists
         '''
-        # lilo:TODO
+        db_name = 'states'
+        table_name = 'states'
+
+        # create (states) database if not exists
         cursor = self.cnx.cursor()
-        sql = 'CREATE DATABASE IF NOT EXISTS states DEFAULT CHARACTER SET = "utf8mb4";'
-        cursor.execute(sql)
-        sql = 'USE states;'
-        cursor.execute(sql)
-        sql = 'CREATE TABLE IF NOT EXISTS states (timestamp INT PRIMARY KEY, state INT DEFAULT 0);'
+        sql = f'CREATE DATABASE IF NOT EXISTS {db_name} DEFAULT CHARACTER SET = "utf8mb4";'
         cursor.execute(sql)
 
+        # create (states) table if not exists
+        sql = 'USE states;'
+        cursor.execute(sql)
+        sql = f'CREATE TABLE IF NOT EXISTS {table_name} (timestamp INT PRIMARY KEY, state INT DEFAULT 0);'
+        cursor.execute(sql)
 
     def update_state(self, timestamp: int, state: int):
         '''
         table schema: [timestamp, state]
+        '''
+        # lilo:TODO
+        pass
+
+    def get_state(self, timestamp: int):
+        '''
+        get the state of a single timestamp
+        return state
+        '''
+        # lilo:TODO
+        pass
+
+    def get_states(self, start: int, end: int):
+        '''
+        get the states for a time range
+        return a list of pairs (timestamp, state)
         '''
         # lilo:TODO
         pass
